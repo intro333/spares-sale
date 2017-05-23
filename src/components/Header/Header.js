2a4d477869b2f5c017383f447ecb37909e1ff80d
intro333 <intro333@ya.ru>
2017-05-23 23:41:16 +0300

    second commit

---
+++ src/components/Header/Header.js
@@ -0,0 +1,10 @@
+import React, { Component } from 'react';
+import './header.css';
+
+export default function (props) {
+    return (
+        <div className="container hero-unit hero-medium background image0">
+            <img class="mobile-image" src="//d1hu588lul0tna.cloudfront.net/toyotaone/ruru/2247-854-6_tcm-3020-718328.jpg" />
+        </div>
+    );
+}
