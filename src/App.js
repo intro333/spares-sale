c2924323772cfd13f59e9563fc381aee19c72feb
dderzhaev <dderzhaev@iMac-Alexander.local>
2017-05-24 19:42:14 +0300

    3

--- src/App.js
+++ src/App.js
@@ -3,13 +3,15 @@ import './App.css';
 import Header from "./components/Header/Header";
 
 class App extends Component {
-  render() {
-    return (
-      <div className="App">
-          <Header />
-      </div>
-    );
-  }
+    render() {
+        return (
+            <div id="html52">
+                <div className="spares-container">
+                    <Header />
+                </div>
+            </div>
+        );
+    }
 }
 
 export default App;
--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -1,10 +1,17 @@
-import React, { Component } from 'react';
-import './header.css';
+import React from 'react';
+import '../../theme/style/main.css';
 
 export default function (props) {
     return (
-        <div className="container hero-unit hero-medium background image0">
-            <img class="mobile-image" src="//d1hu588lul0tna.cloudfront.net/toyotaone/ruru/2247-854-6_tcm-3020-718328.jpg" />
-        </div>
+        <section id="mainfocus" className="page-header-default mainfocus-medium">
+            <div className="container hero-unit hero-medium background image0">
+                <img className="mobile-image" src="//d1hu588lul0tna.cloudfront.net/toyotaone/ruru/2247-854-6_tcm-3020-718328.jpg" alt=""/>
+            </div>
+            <header className="container page-title   medium-article  header-center header-dark">
+                <h2>Мы сделали обслуживание доступнее</h2>
+                <h2>Ради вашей безопасности</h2>
+                <h2>Для вашего комфорта</h2>
+            </header>
+        </section>
     );
 }
